dbd6bab41c64ea2157022b57706d6ea89ca69ca8
Hazufe <fschavezc@gmailcom>
2022-06-15 13:18:15 -0500

    seg

---
+++ src/componentes/lista.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const List = ()=>{
+    const [allProjects, setAllProjects] = React.useState([])
+
+    React.useEffect(() => {
+        async function getProjects(){
+            const res = await fetch("https://task-node-api-rest.herokuapp.com/tasks")
+            const data = await res.json()
+            setAllProjects(data)
+
+        }
+        getProjects()
+    }, [])
+
+    return(
+        <div className="card mt-3">
+            <div className="card-header">
+                
+            </div>
+            
+            <div className="card-body">
+                <h4> Project List</h4>
+               
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Priority</th>
+                            <th>Description</th>
+                         
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+                          
+                       {allProjects.map((project) => (
+
+                        
+                        <tr key={project.id}>
+                            <td>{project.id}</td>
+                            <td>{project.name}</td>
+                            <td>{project.priority}</td>
+                            <td>{project.description}</td>
+                            
+                        </tr>
+                        ))}
+                    </tbody>
+                       
+                </table>
+                 </div> 
+            <div className="card-footer text-muted">
+
+            </div>
+        </div>
+    )
+}
+export default List
